b19c478542cb40e7fc15b09d4bd1fc5c5bbb96fe
Idan Davidi <eidan123455@gmail.com>
2015-04-29 16:56:58 +0300

    The structure of the form has change due to the adiition of error divs. Javascript code has been added in order to:   1. Validity check   2. Closing the login section
     Javascript exercise seems to be finished.

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,7 +1,72 @@
 /**
  * Created by idan on 29/04/15.
  */
-function disappearObject(id) {
-    var object = document.getElementById(id);
-    object.style.display = 'none';
+
+// EventListeners
+function onLoad() {
+    var object;
+
+    // OnClick event listener to hide the login section
+    object = document.getElementById('btnLoginClose');
+    object.addEventListener('click', function () {
+        hideElement('loginSection');
+    });
+
+    // OnClick event to login button
+    object = document.getElementById('login-button');
+    object.addEventListener('click', function() {
+        sendForm()
+    });
+}
+
+function hideElement(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+function sendForm() {
+    var boolContinue = false;
+
+    // UserName check
+    var userNameReg = /[^\da-z!\-]/;
+    var userName = document.getElementById('username');
+    var username_error = document.getElementById('username-error');
+    if (userName.value.length === 0) {
+        username_error.style.display = 'block';
+        username_error.innerHTML = 'יש להזין שם משתמש';
+    }
+    else if (userNameReg.test(userName.value)) {
+        username_error.style.display = 'block';
+        username_error.innerHTML = 'שם משתמש מכיל תווים אסורים';
+    }
+    else {
+        username_error.style.display = 'none';
+        boolContinue = true;
+    }
+
+    // Password Check
+    var password = document.getElementById('password');
+    var password_error = document.getElementById('password-error');
+
+    if (password.value.length === 0) {
+        password_error.style.display = 'block';
+        password_error.innerHTML = 'יש להזין סיסמא';
+    }
+    else if (password.value.length > 8) {
+        password_error.style.display = 'block';
+        password_error.innerHTML = 'סיסמא אסורה: עליך להזין עד 8 תווים';
+    }
+    else if (password.value.indexOf(' ') > -1) {
+        password.style.display = 'block';
+        password_error.innerHTML = 'סיסמא אסורה: עליך להזין תווים ללא רווחים';
+    }
+    else {
+        password_error.style.display = 'none';
+        boolContinue = boolContinue && true;
+    }
+
+    // If connection was successful
+    if (boolContinue) {
+        var loginSection = document.getElementById('loginSection');
+        loginSection.innerHTML = 'התחברת בהצלחה!'
+    }
 }
